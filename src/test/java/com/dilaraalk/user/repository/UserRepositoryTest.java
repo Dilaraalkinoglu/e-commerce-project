b7121a9708496a30fdf6c488e13b46640b7ae56d
Dilara Alkınoğlu <dilaraalkinoglu80@gmail.com>
2026-02-03 20:09:00 +0300

    fix(test): use 'test' profile in UserRepositoryTest to load H2 config

--- src/test/java/com/dilaraalk/user/repository/UserRepositoryTest.java
+++ src/test/java/com/dilaraalk/user/repository/UserRepositoryTest.java
@@ -4,12 +4,14 @@ import com.dilaraalk.user.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
