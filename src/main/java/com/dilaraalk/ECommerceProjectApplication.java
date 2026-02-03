3f33b323e9f188ce7fe0b022b42b2741467b5087
Dilara Alkınoğlu <dilaraalkinoglu80@gmail.com>
2026-02-03 20:38:22 +0300

    refactor: remove redundant ComponentScan and EntityScan annotations to fix slice testing

--- src/main/java/com/dilaraalk/ECommerceProjectApplication.java
+++ src/main/java/com/dilaraalk/ECommerceProjectApplication.java
@@ -2,15 +2,9 @@ package com.dilaraalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EntityScan(basePackages = { "com.dilaraalk" })
-@ComponentScan(basePackages = { "com.dilaraalk" })
-@EnableJpaRepositories(basePackages = "com.dilaraalk")
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
